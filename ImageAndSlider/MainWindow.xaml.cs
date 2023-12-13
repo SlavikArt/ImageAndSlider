01c807b42ca80b73845e7d0e8d503134602c7084
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2023-12-13 16:10:26 +0200

    + gallery

--- ImageAndSlider/MainWindow.xaml.cs
+++ ImageAndSlider/MainWindow.xaml.cs
@@ -23,6 +23,9 @@ namespace ImageAndSlider
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string[] imagePaths;
+        private int currentIndex = 0;
+
         private TransformGroup transformGroup;
         private ScaleTransform scaleTransform;
         private RotateTransform rotateTransform;
@@ -30,6 +33,19 @@ namespace ImageAndSlider
         public MainWindow()
         {
             InitializeComponent();
+
+            imagePaths = new string[]
+            {
+                "https://wallpapers.com/images/high/cat-pictures-9myur9v0ca86msmx.webp",
+                "https://wallpapers.com/images/high/beautiful-cats-1600-x-1000-picture-hrwwwvyup3q0vmwg.webp",
+                "https://wallpapers.com/images/high/beautiful-cats-1080-x-1920-picture-kebu9tec6x6sknza.webp",
+                "https://wallpapers.com/images/high/beautiful-cats-2200-x-1467-picture-913bua4vrxbkagve.webp",
+                "https://wallpapers.com/images/high/beautiful-cats-1433-x-924-picture-x5cgjadw65p7t6h1.webp",
+                "https://wallpapers.com/images/high/beautiful-cats-2121-x-1414-picture-xfbykidls2i9vnw0.webp",
+                "https://wallpapers.com/images/high/beautiful-cats-2545-x-1692-picture-yv0yggf2gr811xd5.webp",
+                "https://wallpapers.com/images/high/beautiful-cats-1800-x-1200-picture-o1fx1eprdzh5j74t.webp"
+            };
+            LoadImage(currentIndex);
 
             transformGroup = new TransformGroup();
             scaleTransform = new ScaleTransform();
@@ -43,30 +59,73 @@ namespace ImageAndSlider
             transformGroup.Children.Add(rotateTransform);
             transformGroup.Children.Add(skewTransform);
 
+            InitializeSliders();
+        }
+
+        private void LoadImage(int index)
+        {
+            if (index >= 0 && index < imagePaths.Length)
+                image.Source = new BitmapImage(new Uri(imagePaths[index]));
+        }
+
+        private void PreviousButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                LoadImage(currentIndex);
+            }
+            else if (currentIndex == 0)
+            {
+                currentIndex = imagePaths.Length - 1;
+                LoadImage(currentIndex);
+            }
+        }
+
+        private void NextButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (currentIndex < imagePaths.Length - 1)
+            {
+                currentIndex++;
+                LoadImage(currentIndex);
+            }
+            else if (currentIndex == imagePaths.Length - 1)
+            {
+                currentIndex = 0;
+                LoadImage(currentIndex);
+            }
+        }
+
+        private void InitializeSliders()
+        {
             sliderBlur.Minimum = 0;
             sliderBlur.Maximum = 50;
-            sliderBlur.ValueChanged += (s, e) => { 
-                image.Effect = new BlurEffect { Radius = e.NewValue }; 
+            sliderBlur.ValueChanged += (s, e) =>
+            {
+                image.Effect = new BlurEffect { Radius = e.NewValue };
             };
 
             sliderOpacity.Minimum = 0;
             sliderOpacity.Maximum = 1;
             sliderOpacity.Value = 1;
-            sliderOpacity.ValueChanged += (s, e) => { 
-                image.Opacity = e.NewValue; 
+            sliderOpacity.ValueChanged += (s, e) =>
+            {
+                image.Opacity = e.NewValue;
             };
 
             sliderRotate.Minimum = 0;
             sliderRotate.Maximum = 360;
             sliderRotate.Value = 0;
-            sliderRotate.ValueChanged += (s, e) => { 
+            sliderRotate.ValueChanged += (s, e) =>
+            {
                 rotateTransform.Angle = e.NewValue;
             };
 
             sliderScale.Minimum = 0.1;
             sliderScale.Maximum = 2;
             sliderScale.Value = 1;
-            sliderScale.ValueChanged += (s, e) => {
+            sliderScale.ValueChanged += (s, e) =>
+            {
                 scaleTransform.ScaleX = e.NewValue;
                 scaleTransform.ScaleY = e.NewValue;
             };
@@ -74,7 +133,8 @@ namespace ImageAndSlider
             sliderSkew.Minimum = -45;
             sliderSkew.Maximum = 45;
             sliderSkew.Value = 0;
-            sliderSkew.ValueChanged += (s, e) => {
+            sliderSkew.ValueChanged += (s, e) =>
+            {
                 skewTransform.AngleX = e.NewValue;
                 skewTransform.AngleY = e.NewValue;
             };
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# My Image Editor
+# My Image Editor (And Gallery)
+
+_Gallery + Editor_
 
 <img src="images/1.png" width="100%">
+<img src="images/2.png" width="100%">
+<img src="images/3.png" width="100%">
+<img src="images/4.png" width="100%">
+<img src="images/5.png" width="100%">
